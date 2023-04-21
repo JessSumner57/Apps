583d5cc1775b34b1e83794b96bc1597e12d7a739
Jess <102857052+JessSumner57@users.noreply.github.com>
2023-04-21 09:31:55 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,90 +1,33 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+function getAverageEmissions(countryCode) {
+  let url = `https://api.v2.emissions-api.org/api/v2/carbon_footprints/average.json?country=${countryCode}&scope=electricity&measure=kwh`;
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.carbon_g_per_kwh)
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
+function calculateCarbonEmissions(distance, modeOfTransport) {
+  let countryCode = "US"; // replace with the appropriate country code based on user input
+  let averageEmissionsPromise = getAverageEmissions(countryCode);
 
-  celsiusTemperature = response.data.main.temp;
-
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+  averageEmissionsPromise.then((averageEmissions) => {
+    let carbonEmissions = distance * modeOfTransport * averageEmissions;
+    let emissionsElement = document.querySelector("#emissions");
+    emissionsElement.innerText = carbonEmissions;
+  });
 }
 
-function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
-}
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-search("New York");
+document.addEventListener("DOMContentLoaded", function () {
+  let form = document.querySelector("#transport-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let distanceElement = document.querySelector("#distance");
+    let modeOfTransportElement = document.querySelector("#mode-of-transport");
+    let distance = distanceElement.value;
+    let modeOfTransport = modeOfTransportElement.value;
+    calculateCarbonEmissions(distance, modeOfTransport);
+  });
+});
